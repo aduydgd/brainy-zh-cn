d23c69d13b56090357c13de996b41eca2de92301
louis <62134021+aduydgd@users.noreply.github.com>
2021-10-30 18:36:20 +0800

    Update webserver_config.php

--- zh-cn/webserver_config.php
+++ zh-cn/webserver_config.php
@@ -1,13 +1,13 @@
 <? //webserver_config
 $lang = array(
-	'webserver_config'		=> 'Webserver VHOST configurator',
-    'webserver_config_mesage'		=> 'Редактировать',
-    'webserver_config_ok'		=> 'Данные успешно сохранены',	
-    'webserver_config_eror'		=> 'Конфигурация с ошибкой',
-    'webserver_config_select_user'		=> 'Выберите пользователя',
-    'webserver_config_save'		=> 'Сохранить',
-    'webserver_config_no_enginx'		=> 'webserver_config Недоступен',
-    'webserver_config_close'		=> 'Закрыть',	
-	'webserver_no_domains'		=> 'Нет доступных доменов',
-    'webserverconfig_paramwitherror' => 'Параметры содержат ошибку',
+	'webserver_config' => 'Webserver VHOST 配置器',
+     'webserver_config_mesage' => '编辑',
+     'webserver_config_ok' => '数据保存成功',
+     'webserver_config_eror' => '配置错误',
+     'webserver_config_select_user' => '选择用户',
+     'webserver_config_save' => '保存',
+     'webserver_config_no_enginx' => 'webserver_config 不可用',
+     'webserver_config_close' => '关闭',
+'webserver_no_domains' => '没有可用的域',
+     'webserverconfig_paramwitherror' => '参数包含错误',
 );
