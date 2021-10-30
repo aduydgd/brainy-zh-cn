83208c9d9f343dc750ff99831aeaf8d9933db26e
louis <62134021+aduydgd@users.noreply.github.com>
2021-10-30 18:39:21 +0800

    Update web_cache.php

--- zh-cn/web_cache.php
+++ zh-cn/web_cache.php
@@ -1,20 +1,20 @@
 <?php
 
 $lang = array(
-    'web_cache' => 'Кэширование',
-    'web_cacheDomain' => 'Домен',
-    'web_cacheExtTh' => 'Расширения для отдачи статики Nginx',
-    'web_cacheExpiresTh' => 'Время кэширования',
-    'web_cacheActionTh' => 'Действия',
-    'web_cacheHint' => 'Укажите расширения через ; , | или пробел',
-    'web_cacheExpHint' => 'Например: 5m',
-    'web_cacheHintSave' => 'Сохранить',
-    'web_cacheHintNoDom' => 'У пользователя нет доменов',
-    'web_cacheHintWrongUser' => 'Неверный формат пользователя',
-    'web_cacheHintWrongExt' => 'Список расширений указан в неверном формате',
-    'web_cacheHintWrongExtExp' => 'Время кэширования указано в неверном формате',
-    'web_cacheHintSaveok' => 'Сохранено успешно',
-    'web_cacheWaitPlease' => 'Пожалуйста, подождите',
+   'web_cache' => '缓存',
+    'web_cacheDomain' => '域',
+    'web_cacheExtTh' => '服务 Nginx 静态的扩展',
+    'web_cacheExpiresTh' => '缓存时间',
+    'web_cacheActionTh' => '动作',
+    'web_cacheHint' => '指定扩展名； , |或空格 ',
+    'web_cacheExpHint' => '例如：5m',
+    'web_cacheHintSave' => '保存',
+    'web_cacheHintNoDom' => '用户没有域',
+    'web_cacheHintWrongUser' => '无效的用户格式',
+    'web_cacheHintWrongExt' => '扩展名列表格式错误',
+    'web_cacheHintWrongExtExp' => '缓存时间格式不正确',
+    'web_cacheHintSaveok' => '保存成功',
+    'web_cacheWaitPlease' => '请稍候',
 
 );
     ?>
