1798ffdbeff1f833396da5fcade8b4a462032b0a
louis <62134021+aduydgd@users.noreply.github.com>
2021-10-30 16:31:50 +0800

    Update black_domain.php

--- zh-cn/black_domain.php
+++ zh-cn/black_domain.php
@@ -1,12 +1,12 @@
 <?php
 $lang = array(
-	'black_domain'		=> 'Запрещенные домены',
-	'black_domain_list'		=> 'Список запрещенных доменов',	
-	'black_domain_edit'		=> 'Сохранить',
-	'black_domain_error_domain'		=> 'Добавляемый домен некорректный',
-	'black_domain_succes'		=> 'Успешно выполнено',	
-	'black_domain_black'		=> 'Добавляемый домен находится в черных списках',
-	'black_domain_edit_black'		=> 'Редактируемый домен находится в черных списках',
-    'black_domain_adddenydomain' => 'Добавить запрещенные домены',
-    'black_domain_forbidden_add' => 'Запрет на добавление хостаккаунтами, доменов перечисленных в данном модуле.',
+	'black_domain' => '阻止域',
+'black_domain_list' => '阻止的域列表',
+'black_domain_edit' => '保存',
+'black_domain_error_domain' => '添加的域无效',
+'black_domain_succes' => '成功完成',
+'black_domain_black' => '要添加的域在黑名单中',
+'black_domain_edit_black' => '正在编辑的域被列入黑名单',
+     'black_domain_adddenydomain' => '添加被阻止的域',
+     'black_domain_forbidden_add' => '禁止通过主机帐户添加此模块中列出的域',
 );
