3946876249115579d33a8d2d263a2e1ca0c2a4c7
louis <62134021+aduydgd@users.noreply.github.com>
2021-10-31 09:03:43 +0800

    Update stream_show.php

--- zh-cn/stream_show.php
+++ zh-cn/stream_show.php
@@ -1,8 +1,8 @@
 <?php
 
 $lang = array(
-    'stream_show'           => 'Отображение потоков',
-    'clear_script'          => 'Чистый скрипт'
+    'stream_show'           => '显示数据流',
+    'clear_script'          => '清除脚本'
 );
 
 ?>
