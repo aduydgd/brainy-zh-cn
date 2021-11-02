272ec0fa78855cc12cd742e7f94bbb8ce8b84bf6
louis <62134021+aduydgd@users.noreply.github.com>
2021-11-02 18:06:11 +0800

    Update http2.php

--- zh-cn/http2.php
+++ zh-cn/http2.php
@@ -1,23 +1,23 @@
 <?php
 
 $lang = array(
-    'http2_manager' => 'Управление протоколом HTTP/2 для виртуальных хостов',
-    'http2_control_seluser' => 'Выберите пользователя',
-    'http2_control_domain' => 'Домен',
-    'http2_control_apache' => 'Текущее состояние',
-    'http2_control_nginx' => 'Текущее для nginx',
-    'http2_control_DomainDomainNotExist' => 'У пользователя нет доменов',
-    'http2_control_DomainWrongUser' => 'Неверное имя пользователя',
-    'http2_control_apacheDomainSetOk' => 'Протокол HTTP2 был успешно изменен.',
-    'http2_control_nginxDomainSetOk' => 'Протокол HTTP2 был успешно изменен.',
-    'menu_http2' => 'Управление протоколом HTTP2',
-    'index_http2' => 'Управление HTTP/2',
-    'http2_control_hintApache' => 'включение/выключение протокола HTTP/2 для Apache',
-    'http2_control_hintNGinx' => 'включение/выключение протокола HTTP/2 для NGinx',
-    'http2_host_all' => 'Устанавливать для новых хостов.',
-    'http2_control_host_all' => 'Изменения были успешно выполнены.',
-    'http2_warning' => 'Внимание! Для Apache протокол HTTP/2 работает только на MPM Worker и Event, если он стоит как Frontend Server. Игнорируйте это сообщение если это не так. Протокол HTTP/2 работает на SSL.',
-	'http2_domains' => 'Домены'
+'http2_manager' => '虚拟主机的 HTTP/2 协议管理',
+    'http2_control_seluser' => '选择用户',
+    'http2_control_domain' => '域',
+    'http2_control_apache' => '当前状态',
+    'http2_control_nginx' => '当前用于 nginx',
+    'http2_control_DomainDomainNotExist' => '用户没有域',
+    'http2_control_DomainWrongUser' => '无效用户名',
+    'http2_control_apacheDomainSetOk' => 'HTTP2 协议更改成功',
+    'http2_control_nginxDomainSetOk' => 'HTTP2 更改成功',
+    'menu_http2' => 'HTTP2 协议管理',
+    'index_http2' => 'HTTP/2 控制',
+    'http2_control_hintApache' => '启用/禁用 Apache 的 HTTP/2 协议',
+    'http2_control_hintNGinx' => '启用/禁用 NGinx 的 HTTP/2 协议',
+    'http2_host_all' => '为新主机设置。',
+    'http2_control_host_all' => '更改已成功完成。',
+    'http2_warning' => '注意！对于 Apache，HTTP / 2 协议仅适用于 MPM Worker 和 Event，如果它代表前端服务器。如果不是，请忽略此消息。 HTTP / 2 通过 SSL 运行。',
+'http2_domains' => '域'
 );
 
 ?>
