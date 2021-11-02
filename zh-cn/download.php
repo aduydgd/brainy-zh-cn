2cfdd6def739ea89355afcb4567bd389ae6b9367
louis <62134021+aduydgd@users.noreply.github.com>
2021-11-02 19:31:43 +0800

    Update download.php

--- zh-cn/download.php
+++ zh-cn/download.php
@@ -1,9 +1,9 @@
 <?php
 
 $lang = array(
-	'download'		=> 'Загрузки',
-    'download_not_root'		=> 'У вас недостаточно прав!',
-    'download_not_file'		=> 'Файл не задан!',
+	'download'		=> '下载',
+    'download_not_root'		=> '你没有足够的权限!',
+    'download_not_file'		=> '未指定文件!',
 	
 );
 
